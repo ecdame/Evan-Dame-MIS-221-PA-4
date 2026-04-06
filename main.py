14b280769f8a6f9af2313ae6942a8c6bcceddd67
Evan Dame <ecdame@crimson.ua.edu>
2026-04-06 01:35:06 -0500

    v4

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ with open(filepath, "r", encoding="utf-8-sig") as f:
         player = Player(stats[0], stats[1], stats[3], stats[4], stats[28], stats[22], stats[23])
         player_list.append(player)
 
+print("Welcome to the NBA stat tracker program!\n" \
+"This program allows you to query a database of NBA players, and find several relevant statistics for them.\n" \
+"To execute a command, please type the name of the command when prompted in the main menu, and follow all instructions.")
 # Start the menu-driven part of the program once all data is loaded.
 utilities.menu(player_list)
 
